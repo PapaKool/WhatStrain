2431b6e88dc5f29522fefb58942f7432b8c66e84
PapaKool <56418824+PapaKool@users.noreply.github.com>
2021-09-21 07:39:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def getstraininfo(query):  # Gets variables to plug into the DM
 
   try:
     info = urlopen(link)  # Grab html
-  except HTTPError as e:
+  except urllib.error.HTTPError as e:
     content = e.read()
     print(content)
   bssearch = bs(info, 'html.parser')  # Converts to BeautifulSoup object
