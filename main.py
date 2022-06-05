670b87552d591f3d0cce1d6388bd893627969c95
PapaKool <56418824+PapaKool@users.noreply.github.com>
2022-06-04 21:32:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from discord import Client, Intents, Embed, Activity, ActivityType
-import interactions
-from interactions.utils.manage_commands import create_option, create_choice
-from interactions.utils.manage_components import *
+import discord_interactions as interactions
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
+from discord_slash.utils.manage_components import *
 import os
 import asyncio
 import re
