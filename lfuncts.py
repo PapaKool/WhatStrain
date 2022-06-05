03a50967c857f15015036da07c1044aa2457778c
PapaKool <56418824+PapaKool@users.noreply.github.com>
2022-06-04 21:46:59 -0400

    Update lfuncts.py

--- lfuncts.py
+++ lfuncts.py
@@ -27,7 +27,7 @@ async def leaflyinfo(query):  # Gets search results and returns message embed
 
   # results = bssearch.find_all(class_='relative flex flex-col justify-between bg-white h-full elevation-low rounded')
   # Grabs each 'box''s div element
-  results = search(f'{query.lower()} site:leafly.com/strains')
+  results = search(term=f'{query.lower()} site:leafly.com/strains', advanced=True)
   for result in results:
     
     
